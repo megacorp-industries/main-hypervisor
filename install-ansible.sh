cbc61a3fa2e6187edd89dc18783503d73d76f491
megacorp-industries <121791047+megacorp-industries@users.noreply.github.com>
2023-01-10 13:38:35 +0000

    Update install-ansible.sh

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
+
+if apt list --installed | grep -q ansible; then
+  echo "======================================"
+  echo "Removing ansible installed through apt"
+  echo "======================================"
+  sudo apt remove -y ansible
+fi
+
+export PATH="$HOME/.local/bin:$PATH"
+python3 -m pip install --user ansible
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py --user
+rm get-pip.py
+python3 -m pip install --user ansible
